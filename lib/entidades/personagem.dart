dc35917507791ee74b7280c307bf3915bd1e631f
Lucas Henrique de Abreu <lucas.abreu@betrybe.com>
2025-08-26 19:46:20 -0300

    COmmit inicial

---
+++ lib/entidades/personagem.dart
@@ -0,0 +1,28 @@
+class Personagem {
+  String nome;
+  int vida;
+  int escudo;
+  int velocidade;
+
+  Personagem(this.nome, this.vida, this.escudo, this.velocidade);
+
+  void defender(int dano) {
+    var danoReal = dano - escudo;
+    if (danoReal > 0) {
+      vida -= danoReal;
+    }
+    if (vida < 0) {
+      vida = 0;
+    }
+  }
+
+  bool estaVivo() {
+    return vida > 0;
+  }
+
+  void atacar(Personagem oponente, int dano) {
+    print('Nome do atacante $nome');
+    print('Nome oponente ${oponente.nome}');
+    oponente.defender(dano);
+  }
+}
