3b854c88be80e8296965575490815f78ed1530df
Lucas Henrique de Abreu <lucas.abreu@betrybe.com>
2025-08-26 21:20:43 -0300

    Ajuste do teste

--- lib/entidades/dado.dart
+++ lib/entidades/dado.dart
@@ -1,1 +1,12 @@
-class Dado {}
+import 'dart:math';
+
+class Dado {
+  final int _lados;
+  Dado(this._lados);
+
+  int get lados => _lados;
+
+  int jogarDado() {
+    return Random().nextInt(_lados) + 1;
+  }
+}
